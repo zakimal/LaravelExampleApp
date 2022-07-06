f24db123155ef7f9e810c6176f9164ae87b69782
Yoichi Ozaki <yoichiozaki@microsoft.com>
2022-07-06 14:10:12 +0900

    update default config

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "Copying custom default.conf over to /etc/nginx/sites-available/default.conf"
+cp /home/site/wwwroot/scripts/default /etc/nginx/sites-available/default
+service nginx reload
 
-NGINX_CONF=/home/site/wwwroot/scripts/default
+# echo "Copying custom default.conf over to /etc/nginx/sites-available/default.conf"
 
-if [ -f "$NGINX_CONF" ]; then
-    cp /home/site/wwwroot/scripts/default /etc/nginx/sites-available/default
-    service nginx reload
-else
-    echo "File does not exist, skipping cp."
-fi
+# NGINX_CONF=/home/site/wwwroot/scripts/default
+
+# if [ -f "$NGINX_CONF" ]; then
+#     cp /home/site/wwwroot/scripts/default /etc/nginx/sites-available/default
+#     service nginx reload
+# else
+#     echo "File does not exist, skipping cp."
+# fi
